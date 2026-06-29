29ac9d67ad3d814863ce90ddbc1b3cb387738073
ROQUE ACOSTA <tsdia.roqueacosta@gmail.com>
2026-06-29 11:22:22 -0300

    Add Home.py for Streamlit project setup

---
+++ app/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Proyecto Integrador", layout="wide")
+
+st.title("Proyecto Integrador - Minería de Datos I")
+
+st.write("Bienvenidos a la aplicación del proyecto.")
